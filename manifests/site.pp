23a378e102dfd87ec4bb11d0dab9685832e2cb25
ac <test@test.com>
2017-03-05 12:04:24 +0000

    Added windows features config and additional chocolatey packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,38 +2,67 @@ include chocolatey
 
 node default {
 
-  if $::kernel == 'windows' {
-    Package { provider => chocolatey, }
-  }
-
+  Package { provider => chocolatey, }
+  
   $chocolateyPackages = [ 'git'
+				  , 'sourcetree'
+				  , 'poshgit'
+				  , 'python'
                   , 'vagrant'
                   , 'virtualbox'
+				  , 'docker'
                   , 'conemu'
                   , 'googlechrome'
                   , 'postman'
+				  , 'phantomjs'
+				  , 'selenium-chrome-driver'
                   , 'winmerge'
+				  , 'grepwin'
+				  , 'pycharm-community'
                   , 'visualstudiocode'
+				  , 'notepadplusplus.install'
+				  , 'linqpad'
+				  , 'nuget.commandline'
+				  , 'autohotkey'
+				  , 'chocolateygui'
+				  , 'slack'
+				  , 'glasswire'
+				  , 'f.lux'
+				  , 'Carbon'
                   , 'unxutils' ]
 
   package { $chocolateyPackages: 
     ensure => 'installed' 
   }
 
-  vcsrepo { 'E:\Github\VagrantBoxes':
+  vcsrepo { 'D:\Github\VagrantBoxes':
     ensure   => present,
     provider => git,
     source   => 'https://github.com/AndrewCritchley/VagrantFiles.git',
   }
 
-  vcsrepo { 'E:\Github\DeveloperMachineConfig':
+  vcsrepo { 'D:\Github\DeveloperMachineConfig':
     ensure   => present,
     provider => git,
     source   => 'https://github.com/AndrewCritchley/WindowsDevelopmentEnvironment.git',
   }
 
-  windows_path {'C:\Program Files\Git\usr\bin':
+  windows_path { 'C:\Program Files\Git\usr\bin':
       ensure => present,
+  }
+  
+  class { 'windows_autoupdate': 
+    no_auto_reboot_with_logged_on_users => '1' 
+  }
+  
+  $windowsFeatures = [ 'Web-Server'
+                  , 'Web-Asp-Net45' ]
+  
+  $windowsFeatures.each |$feature| { 
+      dsc_windowsfeature { $feature: 
+	     dsc_ensure => 'present',
+	     dsc_name => $feature,
+	  }
   }
   
 }
--- setup.ps1
+++ setup.ps1
@@ -1,30 +1,28 @@
+#Requires -RunAsAdministrator
+#Requires -Version 5
 ## Author: Andrew Critchley
 ## Date: 19/12/2016
 ## Description:
 #### Template for my basic development environment
-## Manual Actions:
-#### - https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
-#### - Set execution policy for powershell
-#### - 
 
-## Parameters
-$github_passphrase = Read-Host 'Passphrase for Github SSH Key?' -AsSecureString
-
-# Generate SSH Keys
-mkdir "E:\SSH_KEYS"
-ssh-keygen -t rsa -b 4096 -C "github_andrew" -f E:\ssh_keys\github_andrew -P $github_passphrase
+Set-ExecutionPolicy RemoteSigned
 
 ## Install chocolatey and puppet agent
 
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+refreshenv
+
 choco install puppet-agent -y
 
-## Add puppet modules
-puppet module install --target-dir ./modules puppetlabs-chocolatey --version 2.0.0
-puppet module install --target-dir ./modules basti1302-windows_path --version 0.1.1
-puppet module install --target-dir ./modules puppetlabs-vcsrepo --version 1.0.2
+refreshenv
 
-puppet apply --modulepath ./modules manifests/site.pp --verbose
+## Install puppet modules
 
-vagrant plugin install vagrant-aws
+puppet module install puppetlabs-chocolatey --version 2.0.0
+puppet module install basti1302-windows_path --version 0.1.1
+puppet module install puppetlabs-vcsrepo --version 1.0.2
+puppet module install puppetlabs-dsc --version 1.2.0
+puppet module install puppet-windows_autoupdate --version 1.2.0
+
+puppet apply manifests/site.pp --verbose
